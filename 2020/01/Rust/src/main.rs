07a5c2bc3d8867f1ae0e6c18cc946235a025b186
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-03-22 13:19:38 +0200

    [Rust] Clean up day 01 solution

--- 2020/01/Rust/src/main.rs
+++ 2020/01/Rust/src/main.rs
@@ -10,7 +10,7 @@ fn parse_input(input: String) -> Vec<i32> {
         .collect()
 }
 
-fn two_sum_n(n: i32, xs: &Vec<i32>) -> Option<Vec<i32>> {
+fn two_sum_n(n: i32, xs: &[i32]) -> Option<Vec<i32>> {
     let mut pairs = HashSet::new();
     for x in xs {
         let y = n - x;
@@ -23,8 +23,8 @@ fn two_sum_n(n: i32, xs: &Vec<i32>) -> Option<Vec<i32>> {
     None
 }
 
-fn three_sum_n(n: i32, xs: &Vec<i32>) -> Option<Vec<i32>> {
-    let mut ys = xs.clone();
+fn three_sum_n(n: i32, xs: &[i32]) -> Option<Vec<i32>> {
+    let mut ys = xs.to_owned();
     for x in xs {
         ys.remove(0);
         if let Some(mut result) = two_sum_n(n - x, &ys) {
@@ -35,7 +35,7 @@ fn three_sum_n(n: i32, xs: &Vec<i32>) -> Option<Vec<i32>> {
     None
 }
 
-fn output(path: &String) {
+fn output(path: &str) {
     let parsed = parse_input(fs::read_to_string(&path).unwrap());
     let prod = |vector: Vec<i32>| -> i32 { vector.iter().product() };
     let part_1 = prod(two_sum_n(2020, &parsed).unwrap());
