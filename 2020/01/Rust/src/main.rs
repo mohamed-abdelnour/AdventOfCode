09cc6c0005874c05bf90815210d67f81d8520bf5
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-03-22 12:41:47 +0200

    [Rust] Add day 01 solution

---
+++ 2020/01/Rust/src/main.rs
@@ -0,0 +1,55 @@
+use std::collections::HashSet;
+use std::env;
+use std::fs;
+
+fn parse_input(input: String) -> Vec<i32> {
+    input
+        .lines()
+        .filter(|x| !x.is_empty())
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect()
+}
+
+fn two_sum_n(n: i32, xs: &Vec<i32>) -> Option<Vec<i32>> {
+    let mut pairs = HashSet::new();
+    for x in xs {
+        let y = n - x;
+        if pairs.contains(&y) {
+            return Some(vec![*x, y]);
+        } else {
+            pairs.insert(x);
+        }
+    }
+    None
+}
+
+fn three_sum_n(n: i32, xs: &Vec<i32>) -> Option<Vec<i32>> {
+    let mut ys = xs.clone();
+    for x in xs {
+        ys.remove(0);
+        if let Some(mut result) = two_sum_n(n - x, &ys) {
+            result.push(*x);
+            return Some(result);
+        }
+    }
+    None
+}
+
+fn output(path: &String) {
+    let parsed = parse_input(fs::read_to_string(&path).unwrap());
+    let prod = |vector: Vec<i32>| -> i32 { vector.iter().product() };
+    let part_1 = prod(two_sum_n(2020, &parsed).unwrap());
+    let part_2 = prod(three_sum_n(2020, &parsed).unwrap());
+
+    println!("File: {}", path);
+    println!("  Part 1: {}", part_1);
+    println!("  Part 2: {}", part_2);
+}
+
+fn main() {
+    env::args()
+        .skip(1)
+        .collect::<Vec<_>>()
+        .iter()
+        .for_each(|x| output(x));
+}
